9e4c4513768cf5a6cfaca54719caa41f24537309
bohr.qiu <bohr.qiu@gmail.com>
2018-09-27 14:25:27 +0800

    1. 增加生成mybatis mapper.xml

--- src/main/java/com/acooly/coder/module/impl/MyBatisDaoModuleGenerator.java
+++ src/main/java/com/acooly/coder/module/impl/MyBatisDaoModuleGenerator.java
@@ -1,36 +1,43 @@
 package com.acooly.coder.module.impl;
 
 import com.acooly.coder.config.GenerateConfig;
-import com.acooly.coder.module.GenerateKeys;
 import com.acooly.coder.generate.GenerateContext;
 import com.acooly.coder.module.AbstractModuleGenerator;
+import com.acooly.coder.module.GenerateKeys;
 
 /**
  * Domain generator
- * 
+ *
  * @author zhangpu
- * 
  */
 public class MyBatisDaoModuleGenerator extends AbstractModuleGenerator {
 
-	{
-		templateName = "mybatisDao.ftl";
-	}
+    {
+        templateName = "mybatisDao.ftl,mybatisMapper.ftl";
+    }
 
-	@Override
-	protected String getOutputPath(GenerateContext generateContext, String temp) {
-		GenerateConfig cfg = getGenerateConfiguration();
-		String packagePath = getPackagePath(generateContext.getNameScheme().getDaoPackage());
-		return cfg.getWorkspace() + "/" + cfg.getCodePath() + "/" + packagePath;
-	}
+    @Override
+    protected String getOutputPath(GenerateContext generateContext, String temp) {
+        if ("mybatisDao.ftl".equals(temp)) {
+            GenerateConfig cfg = getGenerateConfiguration();
+            String packagePath = getPackagePath(generateContext.getNameScheme().getDaoPackage());
+            return cfg.getWorkspace() + "/" + cfg.getCodePath() + "/" + packagePath;
+        }
 
-	@Override
-	protected String getOutputFile(GenerateContext generateContext, String temp) {
-		return generateContext.getNameScheme().getDaoClassName() + ".java";
-	}
+        if ("mybatisMapper.ftl".equals(temp)) {
+            GenerateConfig cfg = getGenerateConfiguration();
+            return cfg.getWorkspace() + "/" +"src/main/resources/mybatis";
+        }
+        throw new UnsupportedOperationException("x");
+    }
 
-	@Override
-	public String getGenerateKey() {
-		return GenerateKeys.mybatis.name();
-	}
+    @Override
+    protected String getOutputFile(GenerateContext generateContext, String temp) {
+        return generateContext.getNameScheme().getDomainClassName() + "Mapper.xml";
+    }
+
+    @Override
+    public String getGenerateKey() {
+        return GenerateKeys.mybatis.name();
+    }
 }
