ecf2fcdd0ad3feaf31a8e5f4b62b967d18a3a70e
zhangpu <zhangpu@prosysoft.com>
2021-02-18 00:34:22 +0800

    增加待开发的功能 TODO

--- acooly-coder/src/main/java/com/acooly/coder/generate/CodeGenerateService.java
+++ acooly-coder/src/main/java/com/acooly/coder/generate/CodeGenerateService.java
@@ -7,6 +7,8 @@ import com.acooly.coder.generate.event.AcoolyCoderListener;
  * 代码生成服务接口
  *
  * @author zhangpu
+ * TODO: 增加对file上传的支持 2021/2/18
+ * TODO: BUG:采用alisa方式的选项没有在controler中设置mapping
  */
 public interface CodeGenerateService {
 
