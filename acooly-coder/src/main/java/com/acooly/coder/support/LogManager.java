674003cba42295fe59b5f82db4d662de0597b6fa
zhangpu <zhangpu@prosysoft.com>
2023-06-26 16:01:11 +0800

    优化日志，兼容在插件中的处理。

--- acooly-coder/src/main/java/com/acooly/coder/support/LogManager.java
+++ acooly-coder/src/main/java/com/acooly/coder/support/LogManager.java
@@ -17,30 +17,30 @@ import java.util.logging.Logger;
  * @author acooly
  */
 public class LogManager {
-
-    // 初始化LogManager
-    static {
-        System.setProperty("org.freemarker.loggerLibrary","JUL");
-        // 读取配置文件
-        ClassLoader cl = LogManager.class.getClassLoader();
-        InputStream inputStream = null;
-        if (cl != null) {
-            inputStream = cl.getResourceAsStream("log.properties");
-        } else {
-            inputStream = ClassLoader
-                    .getSystemResourceAsStream("log.properties");
-        }
-        java.util.logging.LogManager logManager = java.util.logging.LogManager
-                .getLogManager();
-        try {
-            // 重新初始化日志属性并重新读取日志配置。
-            logManager.readConfiguration(inputStream);
-        } catch (SecurityException e) {
-            System.err.println(e);
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-    }
+//
+//    // 初始化LogManager
+//    static {
+//        System.setProperty("org.freemarker.loggerLibrary","JUL");
+//        // 读取配置文件
+//        ClassLoader cl = LogManager.class.getClassLoader();
+//        InputStream inputStream = null;
+//        if (cl != null) {
+//            inputStream = cl.getResourceAsStream("log.properties");
+//        } else {
+//            inputStream = ClassLoader
+//                    .getSystemResourceAsStream("log.properties");
+//        }
+//        java.util.logging.LogManager logManager = java.util.logging.LogManager
+//                .getLogManager();
+//        try {
+//            // 重新初始化日志属性并重新读取日志配置。
+//            logManager.readConfiguration(inputStream);
+//        } catch (SecurityException e) {
+//            System.err.println(e);
+//        } catch (IOException e) {
+//            System.err.println(e);
+//        }
+//    }
 
     /**
      * 获取日志对象
